23ae3d55e03ef70c92445cb1b552c4f84211a7f1
Porostik <67925568+Porostik@users.noreply.github.com>
2021-12-28 16:27:50 +0300

    remove logs

--- checkFuncs.js
+++ checkFuncs.js
@@ -24,10 +24,6 @@ const checkFiles = (mainConfig, files) => {
 
         if (!_.has(mainConfig, configField)) {
           errors = [...errors, `Error in ${item.path}: ${configField}`];
-        } else {
-          core.info(
-            `Success: ${configField}, ${_.get(mainConfig, configField)}`
-          );
         }
       });
     }
