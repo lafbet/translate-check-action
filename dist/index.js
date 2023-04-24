73da8dfcf80b4038d66e8f32149bc29f2e2a134d
Igor-work <Igor1998298@gmail.com>
2023-04-24 15:47:44 +0300

    LM-2415 - new dist

--- dist/index.js
+++ dist/index.js
@@ -18927,13 +18927,13 @@ module.exports = (mainConfig, files) => {
 const fs = __nccwpck_require__(147);
 
 module.exports = {
-  getJsonFromFile: (path) => JSON.parse(fs.readFileSync(path, "utf8")),
+  getJsonFromFile: async (name, host) => {
+    const response = await fetch(
+      `https://t.lafa.bet/api/locale/result?host=${host}&code=${name}`
+    );
+    const result = await response.json();
 
-  getLabelFromPath: (path) => {
-    const arr = path.split("/");
-    const fileName = arr[arr.length - 1];
-
-    return fileName.match(/([\S]*).json$/)[1];
+    return JSON.parse(result);
   },
 
   getTextFromFile: (path) => fs.readFileSync(path, "utf8"),
@@ -19090,20 +19090,17 @@ const utils = __nccwpck_require__(608);
 const checkConfigs = __nccwpck_require__(194);
 const checkSource = __nccwpck_require__(447);
 
-const getMain = (name, pathToTranslates) => {
-  const result = utils.getJsonFromFile(`${pathToTranslates}/${name}`);
+const getMain = async (name, host) => {
+  const result = await utils.getJsonFromFile(name, host);
 
   return result;
 };
 
-const getConfigs = (mainConfigName, pathToTranslates) => {
-  const result = execSync(`find ${pathToTranslates} -type f -name '*.json'`, {
-    encoding: "utf8",
-  });
+const getConfigs = async (mainConfigName) => {
+  const response = await fetch(`https://t.lafa.bet/api/locale`);
+  const result = Object.keys(await response.json());
 
-  return result
-    .split("\n")
-    .filter((item) => item && utils.getLabelFromPath(item) !== mainConfigName);
+  return result.filter((item) => item !== mainConfigName);
 };
 
 const getFiles = (path) => {
@@ -19115,18 +19112,23 @@ const getFiles = (path) => {
   return result.split("\n").filter(Boolean);
 };
 
-const main = () => {
+const main = async () => {
+  console.log(1111);
   const mainConfigName = core.getInput("main_file");
-  const pathToFiles = core.getInput("path_to_configs");
   const pathSource = core.getInput("source_path");
+  const host = core.getInput("host");
 
-  const mainConfig = getMain(mainConfigName, pathToFiles);
-  const allConfigs = getConfigs(mainConfigName, pathToFiles);
+  const mainConfig = await getMain(mainConfigName, host);
+  const allConfigs = await getConfigs(mainConfigName);
   const sourceFilesPaths = getFiles(pathSource);
 
-  const configsCheckContent = allConfigs.map((item) => ({
-    label: utils.getLabelFromPath(item),
-    langObj: utils.getJsonFromFile(item),
+  const langObj = async (item) => {
+    return await utils.getJsonFromFile(item);
+  };
+
+  const configsCheckContent = allConfigs.map(async (item) => ({
+    label: item,
+    langObj: await langObj(item),
   }));
 
   const filesCheckContent = sourceFilesPaths.map((item) => ({
